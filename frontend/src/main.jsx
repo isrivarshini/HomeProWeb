bb2c0e8ee81e73194485d5e657db470569fdd73b
Sri Varshini Inakollu <94848537+isrivarshini@users.noreply.github.com>
2026-02-18 14:15:37 -0800

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-import './styles/fonts.css'; // Add this line
+//import './styles/fonts.css'; // Add this line
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
